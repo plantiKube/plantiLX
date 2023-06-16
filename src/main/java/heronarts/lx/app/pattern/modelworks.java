4d5158af0999343cb7dedd5758a768f2332d68b8
Nathan Argetsinger <nargetdev@gmail.com>
2023-06-15 23:42:09 -0600

    runs now.. statically selecting the ring module as .. LXModel[] ringChildren = model.children[10].children .. do this more intelligently

--- src/main/java/heronarts/lx/app/pattern/modelworks.java
+++ src/main/java/heronarts/lx/app/pattern/modelworks.java
@@ -2,7 +2,9 @@ package heronarts.lx.app.pattern;
 
 import heronarts.lx.LX;
 import heronarts.lx.LXCategory;
+import heronarts.lx.LXClassLoader;
 import heronarts.lx.color.LXColor;
+import heronarts.lx.model.LXModel;
 import heronarts.lx.model.LXPoint;
 import heronarts.lx.parameter.BooleanParameter;
 import heronarts.lx.parameter.DiscreteParameter;
@@ -16,23 +18,54 @@ public class modelworks extends LXPattern {
     public final BooleanParameter z_en = new BooleanParameter("Z_en", false)
             .setDescription("X plane enabled");
 
-    public final DiscreteParameter x = new DiscreteParameter("select component", 0, this.lx.getModel().children[0].children.length)
+    public final DiscreteParameter x = new DiscreteParameter("select component", 0, 6)
             .setDescription("select segment");
+
     public modelworks(LX lx) {
         super(lx);
 
+        addParameter("x", this.x);
         addParameter("z_en", this.z_en);
     }
 
     public void run(double deltaMs) {
         boolean z_en = this.z_en.getValueb();
-        for (LXPoint p : model.points) {
+        int x = this.x.getValuei();
 
-            if (z_en) {
-                colors[p.index] = LXColor.hsb(0, 100, 100);
+        LXModel model = this.lx.getModel();
+
+        LXModel[] ringChildren = model.children[10].children;
+
+        int i = 0;
+        for (LXModel ring : ringChildren) {
+            if (i == x){
+                for (LXPoint p : ring.points) {
+                    colors[p.index] = LXColor.rgb(
+                            (int) 0xff,
+                            (int) 0xff,
+                            (int) 0xff
+                    );
+                }
             } else {
-                colors[p.index] = LXColor.hsb(120, 100, 100);
+                for (LXPoint p : ring.points) {
+                    colors[p.index] = LXColor.rgb(
+                            (int) 0x00,
+                            (int) 0x00,
+                            (int) 0x00
+                    );
+                }
             }
+
+            i++;
         }
+
+//            for (LXPoint p : model.points) {
+//
+//                if (z_en) {
+//                    colors[p.index] = LXColor.hsb(0, 100, 100);
+//                } else {
+//                    colors[p.index] = LXColor.hsb(120, 100, 100);
+//                }
+//            }
     }
 }
